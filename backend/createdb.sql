d00faaead5196d159386b789b3f28ce44b5770a8
risatrahaman <91422611+risatrahaman@users.noreply.github.com>
2023-04-05 00:44:41 +0600

    update signup

--- backend/createdb.sql
+++ backend/createdb.sql
@@ -1,26 +1,26 @@
 create database ams;
 
 create table user (
-uid int auto_increment not null,
-email varchar(50),
-password varchar(30),
-phone int,
+uid varchar(10),
 fname varchar(15),
 lname varchar(15),
+phone int,
 address varchar(255),
+email varchar(50),
+password varchar(255),
 primary key (uid)
 );
 
 create table admin (
-uid int not null,
-aid int not null,
+uid varchar(10) not null,
+aid varchar(10) not null,
 primary key (aid),
 foreign key (uid) references user(uid)
 );
 
 create table customer (
-uid int not null,
-cid int not null,
+uid varchar(10) not null,
+cid varchar(10) not null,
 distance_covered int,
 type varchar(10),
 primary key (cid),
--- backend/signup-db.php
+++ backend/signup-db.php
@@ -1,15 +1,28 @@
 <?php
-    require("dbconnect.php");
+    require_once("dbconnect.php");
 
+    $uid = $_POST['uid'];
     $fname = $_POST['fname'];
     $lname = $_POST['lname'];
     $phone = $_POST['phone'];
     $address = $_POST['address'];
     $email = $_POST['email'];
     $pass = $_POST['pass'];
+    $encryptedPass = password_hash($pass, PASSWORD_DEFAULT);
 
+    $userMode = $_POST['users'];
+
+    
+    mysqli_query($conn, "insert into user values('$uid', '$fname', '$lname', '$phone', '$address', '$email', '$encryptedPass')");
+    
+    if ($userMode == 'Customer'){
+        mysqli_query($conn, "insert into customer values('$uid', 'C-$uid', 0, 'regular')");
+    }
+    else{
+        mysqli_query($conn, "insert into admin values('$uid', 'A-$uid')");
+    }
+    
     echo "It works!\n";
-    echo "$fname $lname $phone $address $email $pass";
 
 
 ?>
